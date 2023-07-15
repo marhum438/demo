a59750f1af67227bf403433ab6251905bf368d3f
marhum438 <138965889+marhum438@users.noreply.github.com>
2023-07-15 14:07:46 +0500

    done

---
+++ lib/increment.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+class increment extends StatefulWidget {
+  
+  @override
+  State<increment> createState() => _incrementState();
+}
+
+class _incrementState extends State<increment> {
+  int num=02;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp (home: Scaffold(
+
+         
+
+         body:Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                onTap: (){
+                  setState(() {
+                    if(num>1)
+                    {
+                      num--;
+                    }
+                  });
+                },
+                child: Container(
+                  height: 50,
+                  width: 60,
+                 decoration: BoxDecoration(color: Colors.green,borderRadius: BorderRadius.only(bottomLeft: Radius.circular(14),topLeft: Radius.circular(14))),
+                 child: Center(child: Text("-",style: TextStyle(fontSize: 22,),)),
+                        
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 100,
+                decoration: BoxDecoration(color: Color.fromARGB(255, 243, 240, 240)),
+                 child: Center(child: Text("$num",style: TextStyle(fontSize: 22),)),
+              ),
+              InkWell(
+                onTap: (){
+                  setState(() {
+                  
+                    num++;
+                  });
+                },
+                child: Container(
+                   height: 50,
+                  width: 60,
+                  decoration: BoxDecoration(color: Colors.green,borderRadius:BorderRadius.only(bottomRight: Radius.circular(14),topRight: Radius.circular(14))),
+                child: Center(child: Text("+",style: TextStyle(fontSize: 20),)),
+                ),
+              )
+            ],
+          ),
+        
+      )
+    ),
+      
+        
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,41 @@
-import "package:flutter/material.dart";
-import "package:project2/ContactUs.dart";
+import 'package:flutter/material.dart';
+import 'package:project2/ContactUs.dart';
+import 'package:project2/HomeScreen.dart';
+import 'package:project2/demo.dart';
+import 'package:project2/favorite_screen.dart';
+import 'package:project2/increment.dart';
 import 'package:project2/myprofile.dart';
 import 'package:project2/newtask.dart';
 import 'package:project2/privacy_screen.dart';
-import "package:project2/demo.dart";
-import 'package:project2/favorite_screen.dart';
+import 'package:project2/profile_screen.dart';
+import 'package:project2/profile_setting.dart';
 import 'package:project2/settingscreen.dart';
-import 'profile_setting.dart';
-import 'profile_screen.dart';
-import 'HomeScreen.dart';
 
-main() {
-  runApp(MyClass());
+
+void main()
+{
+  runApp(MyApp());
 }
 
-class MyClass extends StatelessWidget {
-  const MyClass({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-         //  home: Demo(),
-        // home: ContactUs(),
-        //home: favoriteScreen(),
-        // home: HomeScreen(),
-      // home: PrivacyScreen(),
-      //home: ProfileScreen(),
-      //home: ProfileSetting(),
-    //  home: SettingScreen(),
-       home:newtask(),
-      //  home:myprofile(),
+      // home: ContactUs(),
+     // home:Demo(),
+    //    home:favoriteScreen(),
+        // home:HomeScreen(),
+         home:increment(),
+          //  home:myprofile(),
+          //  home:newtask(),
+            // home:PrivacyScreen(),
+            //  home:ProfileScreen(),
+            //   home:ProfileSetting(),
+              //  home:SettingScreen(),
+
     );
   }
-
-
 }
-
