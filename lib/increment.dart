2ae7be69f93a09760cd6604c8944502dc67a632d
kashaf172725 <134833348+kashaf172725@users.noreply.github.com>
2023-07-21 15:25:49 -0700

    ........

--- lib/increment.dart
+++ lib/increment.dart
@@ -1,67 +1,81 @@
 import 'package:flutter/material.dart';
+
 class increment extends StatefulWidget {
-  
   @override
   State<increment> createState() => _incrementState();
 }
 
 class _incrementState extends State<increment> {
-  int num=02;
+  int num = 02;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp (home: Scaffold(
-
-         
-
-         body:Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              InkWell(
-                onTap: (){
-                  setState(() {
-                    if(num>1)
-                    {
-                      num--;
-                    }
-                  });
-                },
-                child: Container(
-                  height: 50,
-                  width: 60,
-                 decoration: BoxDecoration(color: Colors.green,borderRadius: BorderRadius.only(bottomLeft: Radius.circular(14),topLeft: Radius.circular(14))),
-                 child: Center(child: Text("-",style: TextStyle(fontSize: 22,),)),
-                        
-                ),
-              ),
-              Container(
+    return MaterialApp(
+      home: Scaffold(
+          body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {
+                setState(() {
+                  if (num > 1) {
+                    num--;
+                  }
+                });
+              },
+              child: Container(
                 height: 50,
-                width: 100,
-                decoration: BoxDecoration(color: Color.fromARGB(255, 243, 240, 240)),
-                 child: Center(child: Text("$num",style: TextStyle(fontSize: 22),)),
+                width: 60,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(14),
+                        topLeft: Radius.circular(14))),
+                child: Center(
+                    child: Text(
+                  "-",
+                  style: TextStyle(
+                    fontSize: 22,
+                  ),
+                )),
               ),
-              InkWell(
-                onTap: (){
-                  setState(() {
-                  
-                    num++;
-                  });
-                },
-                child: Container(
-                   height: 50,
-                  width: 60,
-                  decoration: BoxDecoration(color: Colors.green,borderRadius:BorderRadius.only(bottomRight: Radius.circular(14),topRight: Radius.circular(14))),
-                child: Center(child: Text("+",style: TextStyle(fontSize: 20),)),
-                ),
-              )
-            ],
-          ),
-        
-      )
-    ),
-      
-        
+            ),
+            Container(
+              height: 50,
+              width: 100,
+              decoration:
+                  BoxDecoration(color: Color.fromARGB(255, 243, 240, 240)),
+              child: Center(
+                  child: Text(
+                "$num",
+                style: TextStyle(fontSize: 22),
+              )),
+            ),
+            InkWell(
+              onTap: () {
+                setState(() {
+                  num++;
+                });
+              },
+              child: Container(
+                height: 50,
+                width: 60,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(14),
+                        topRight: Radius.circular(14))),
+                child: Center(
+                    child: Text(
+                  "+",
+                  style: TextStyle(fontSize: 20),
+                )),
+              ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
